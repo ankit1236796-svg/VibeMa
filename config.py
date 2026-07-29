2a338f39334a5a310bf0756808dce109551ea3fa
ankit1236796-svg <ankit1236796@gmail.com>
2026-07-29 23:10:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 # Railway Dashboard > Variables se uthayega
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8271913562:AAGycnacKhwMTcCrzkAsZhudgb-PhIQwWr4")
-PLAYWRIGHT_SCRAPER_URL = os.getenv("PLAYWRIGHT_SCRAPER_URL", "")
+PLAYWRIGHT_SCRAPER_URL = os.getenv("https://loving-liberation-production-52a3.up.railway.app", "")
 
 # Intervals
 CHECK_INTERVAL_SECONDS = int(os.getenv("CHECK_INTERVAL_SECONDS", 180))
